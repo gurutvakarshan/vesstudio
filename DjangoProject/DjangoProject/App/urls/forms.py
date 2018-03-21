d396d3117bc3d3e436dbc6177cb59f4460849e48
gurutvakarshan <repo.opensource@gmail.com>
2018-03-21 23:56:16 +0530

    continuing the previous state of mind.

--- DjangoProject/DjangoProject/App/urls/forms.py
+++ DjangoProject/DjangoProject/App/urls/forms.py
@@ -6,7 +6,7 @@ urlpatterns = patterns('DjangoProject.App.views.forms',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^/$','forms',name='forms'),
-    url(r'^activities/fill_marks/academic/$','level1',name='level1'),
+    url(r'^/activities/fill_marks/academic/$','level1',name='level1'),
     url(r'^/activities/fill_marks/co_curricular/$','level2',name='level2'),
     url(r'^/activities/fill_marks/extra_curricular/$','level3',name='level3'),
     url(r'^/activities/fill_marks/sports/$','level4',name='level4'),
