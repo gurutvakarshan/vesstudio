2f007fc37dc0ba8f02fd3b577858fb8675f298e1
gurutvakarshan <repository.opensource@gmail.com>
2018-09-19 22:58:45 +0530

    mongoengine ORM model for Django

--- DjangoProject/DjangoProject/App/views/methods.py
+++ DjangoProject/DjangoProject/App/views/methods.py
@@ -7,14 +7,14 @@ from django.core.mail import EmailMessage
 
 def retrive(request):
 	if request.user.is_authenticated:
-		user_member_reg = connection.ves_dev.contestants_reg.UserMemberReg()					
+		user_member_reg = UserMemberReg()					
 	else:
     
 	return HttpResponse()
 
 def display(request):
 	if request.user.is_authenticated:
-		user_member_reg = connection.ves_dev.contestants_reg.UserMemberReg()
+		user_member_reg = UserMemberReg()
 	else:
     
 	return HttpResponse()
